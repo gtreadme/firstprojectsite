f4cd3590c55c24eb08cefb5f225e195b512b7115
gtree <gtreece@hotmail.co.nz>
2018-10-12 15:19:44 +1300

    saved url notes

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -7,3 +7,12 @@ urlpatterns = [
     ]
  
 
+##from . import views
+##from django.conf import settings
+##from django.conf.urls.static import static
+
+##urlpatterns = [
+  ##  url('', views.post_list, name='post_list'),
+
+
+##] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
