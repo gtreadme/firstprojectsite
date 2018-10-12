5528203dfed776bc5402dffeac399e5ef30507cc
gtree <gtreece@hotmail.co.nz>
2018-10-12 15:04:30 +1300

    added detailed posts

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-    url('', views.post_list, name='post_list'),
+    url(r'^post/$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    ]
+ 
 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)    
